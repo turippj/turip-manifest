65ecc75c22f251ba6167980cc5cbf1a75d219ee3
MakTak <justaway22@gmail.com>
2017-11-02 21:00:33 +0900

    webappのapp.yamlの記述を修正

--- main_test.py
+++ main_test.py
@@ -19,13 +19,13 @@ class TestHandlers(unittest.TestCase):
         # AddManifest Test
         test_file = open('./test.json', 'r')
         post_contents = {'file_data': test_file.read()}
-        request = webapp2.Request.blank('/api/manifest/add_manifest', POST=post_contents)
+        request = webapp2.Request.blank('/api/manifest/add', POST=post_contents)
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
         self.assertEqual(response.body, 'Upload Manifest success!')
 
         # SearchManifest Test
-        request = webapp2.Request.blank('/api/manifest/search_manifest/65535')
+        request = webapp2.Request.blank('/api/manifest/search/65535')
         response = request.get_response(main.app)
         self.assertEqual(response.status_int, 200)
 
